31ef513c87184b2dc4831e939aa41403891d1ed6
Francesco Montefoschi <francesco.monte@gmail.com>
2015-06-05 10:03:16 +0200

    Refactored UdooArduinoSensor to more specific UdooTempHumSensor

--- appinventor/components/src/com/google/appinventor/components/common/PropertyTypeConstants.java
+++ appinventor/components/src/com/google/appinventor/components/common/PropertyTypeConstants.java
@@ -120,6 +120,13 @@ public class PropertyTypeConstants {
    * com.google.appinventor.client.editor.youngandroid.properties.YoungAndroidUdooTransportsChoicePropertyEditor#transports
    */
   public static final String PROPERTY_TYPE_UDOO_TRANSPORTS = "udoo_transports";
+  
+  /**
+   * UDOO transports
+   * @see
+   * com.google.appinventor.client.editor.youngandroid.properties.YoungAndroidUdooTempHumSensorsChoicePropertyEditor#sensors
+   */
+  public static final String PROPERTY_TYPE_UDOO_TEMP_HUM_SENSORS = "udoo_temp_hum_sensors";
 
   /**
    * Lego EV3 sensor ports.

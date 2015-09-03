9ef56ff50b3c46a864fc7f9498a84306adde8820
Francesco Montefoschi <francesco.monte@gmail.com>
2015-09-03 14:35:03 +0200

    Udoo*Sensor classes are now using UdooArduino to recycle it's ADK/TCP connection, without the need to re-establish a new, independent connection

--- appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
+++ appinventor/appengine/src/com/google/appinventor/client/OdeMessages.java
@@ -6133,6 +6133,10 @@ public interface OdeMessages extends Messages {
   @DefaultMessage("Direction")
   @Description("")
   String DirectionProperties();
+  
+  @DefaultMessage("UdooArduino")
+  @Description("")
+  String UdooArduinoProperties();
 
   @DefaultMessage("SelectionColor")
   @Description("")

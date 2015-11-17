dc30e9d5fe1e6ae58c9a7a3fb64dac2ad39f4571
Francesco Montefoschi <francesco.monte@gmail.com>
2015-11-17 14:53:58 +0100

    Support for Servo.h

--- appinventor/appengine/src/com/google/appinventor/client/Images.java
+++ appinventor/appengine/src/com/google/appinventor/client/Images.java
@@ -1,3 +1,4 @@
+
 // -*- mode: java; c-basic-offset: 2; -*-
 // Copyright 2009-2011 Google, All Rights reserved
 // Copyright 2011-2017 MIT, All rights reserved
@@ -607,4 +608,6 @@ public interface Images extends Resources {
   @Source("com/google/appinventor/images/udooTemperature.png")
   ImageResource udooTemperature();
 
+  @Source("com/google/appinventor/images/udooGear.png")
+  ImageResource udooGear();
 }
